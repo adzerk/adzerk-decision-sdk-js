8835b238f8e7a362e696501fc8a79631a2e61810
Justin Niessner <CrshOverride@gmail.com>
2019-11-22 23:14:23 -0500

    Update tests

--- test/client.test.ts
+++ test/client.test.ts
@@ -1,7 +1,6 @@
 import "jest";
 import { Response } from "node-fetch";
 import Client from "../src";
-import { isDecisionMultiWinner } from "../src/models";
 
 let buildFetchMock = (response: any) =>
   jest.fn(async (url: string, opts: any) => {
@@ -11,6 +10,103 @@ let buildFetchMock = (response: any) =>
       })
     );
   });
+
+let expected = {
+  user: {
+    key: "abc"
+  },
+  decisions: {
+    div1: [
+      {
+        adId: 2104402,
+        creativeId: 1773302,
+        flightId: 2583477,
+        campaignId: 502103,
+        priorityId: 99645,
+        clickUrl: "https://some-click-url",
+        impressionUrl: "https://some-impression-url",
+        contents: [
+          {
+            type: "html",
+            data: {
+              height: 250,
+              width: 300,
+              imageUrl: "https://some-image-url",
+              title: "",
+              fileName: "image.png"
+            },
+            body:
+              '<a href="" rel="nofollow" target="_blank" title=""><img src="" title="" alt="" border="0" width="300" height="250"></a>',
+            template: "image"
+          }
+        ],
+        height: 250,
+        width: 300,
+        events: []
+      }
+    ]
+  }
+};
+
+test("client obeys a protocol override", async () => {
+  let fetch = buildFetchMock({});
+  let client = new Client({
+    networkId: 23,
+    fetch: fetch as any,
+    protocol: "http"
+  });
+
+  await client.getDecisions({ placements: [] });
+
+  expect(fetch.mock.calls.length).toBe(1);
+  expect(fetch.mock.calls[0][0]).toBe("http://e-23.adzerk.net/api/v2");
+});
+
+test("client obeys a host override", async () => {
+  let fetch = buildFetchMock({});
+  let client = new Client({
+    networkId: 23,
+    fetch: fetch as any,
+    host: "ads.some.com"
+  });
+
+  await client.getDecisions({ placements: [] });
+
+  expect(fetch.mock.calls.length).toBe(1);
+  expect(fetch.mock.calls[0][0]).toBe("https://ads.some.com/api/v2");
+});
+
+test("client obeys a path override", async () => {
+  let fetch = buildFetchMock({});
+  let client = new Client({
+    networkId: 23,
+    fetch: fetch as any,
+    path: "/some/crazy/proxy"
+  });
+
+  await client.getDecisions({ placements: [] });
+
+  expect(fetch.mock.calls.length).toBe(1);
+  expect(fetch.mock.calls[0][0]).toBe(
+    "https://e-23.adzerk.net/some/crazy/proxy"
+  );
+});
+
+test("client allows a full url override", async () => {
+  let fetch = buildFetchMock({});
+  let client = new Client({
+    networkId: 23,
+    fetch: fetch as any,
+    protocol: "http",
+    host: "ads.some.com",
+    path: "/some/crazy/proxy"
+  });
+
+  let result = await client.getDecisions({ placements: [] });
+
+  expect(fetch.mock.calls.length).toBe(1);
+  expect(fetch.mock.calls[0][0]).toBe("http://ads.some.com/some/crazy/proxy");
+});
 
 test("client makes simple request and returns single winner response", async () => {
   let response = {
@@ -74,9 +170,7 @@ test("client makes simple request and returns single winner response", async () 
   let body = JSON.parse(fetch.mock.calls[0][1].body);
   expect(body).toEqual(request);
 
-  expect(result).toEqual(response);
-
-  expect(isDecisionMultiWinner(result.decisions)).toBeFalsy();
+  expect(result).toEqual(expected);
 });
 
 test("client makes simple request and returns multi winner response", async () => {
@@ -144,7 +238,5 @@ test("client makes simple request and returns multi winner response", async () =
   let body = JSON.parse(fetch.mock.calls[0][1].body);
   expect(body).toEqual(request);
 
-  expect(result).toEqual(response);
-
-  expect(isDecisionMultiWinner(result.decisions)).toBeTruthy();
+  expect(result).toEqual(expected);
 });
