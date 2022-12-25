0f71cbbbc15ed2295a8bd152ca68742a2a965b61
Vinod Kurup <vinod@kurup.com>
2022-12-25 08:20:49 -0700

    Upgrade to Parcel v2

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,11 @@ export * from './models';
 export { Client };
 
 export interface LoggerFunc {
-  (lvl: 'debug' | 'info' | 'warn' | 'error', msg: string, meta?: object): void;
+    (lvl: 'debug' | 'info' | 'warn' | 'error', msg: string, meta?: object): void;
 }
+
+// Set AdzerkDecisionSdk as the global name for the clientSideApp (browser) build
+declare global {
+    var AdzerkDecisionSdk: object;
+}
+globalThis.AdzerkDecisionSdk = exports;
